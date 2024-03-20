7683ee13f6e1f9537af1ce54ca48c507d42ab5fd
jhontenorio1618 <jhontenorio1618@gmail.com>
2024-03-19 23:58:20 -0500

    initial map

---
+++ Wraithfall/WraithFall Map/TSXFiles/cave_entrance.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="cave_entrance" tilewidth="264" tileheight="191" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="264" height="191" source="../graphics/cave_entrance.png"/>
+ </tile>
+</tileset>
---
+++ Wraithfall/WraithFall Map/TSXFiles/toxicPuddle.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="toxicPuddle" tilewidth="566" tileheight="441" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="566" height="441" source="../graphics/puddle.png"/>
+ </tile>
+</tileset>
