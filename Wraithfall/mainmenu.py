7343dcea12df41d608dbd2889d0255a4e1755dbf
mcrank1 <mcrank@luc.edu>
2024-04-17 16:24:31 -0500

    Added Music asset and added main menu song to asset and added song to main menu.

--- Wraithfall/mainmenu.py
+++ Wraithfall/mainmenu.py
@@ -30,7 +30,10 @@ DEFAULT_BACKGROUND_IMAGE_POSITION = (640,360)
 def get_font(size):
     return pygame.font.Font(os.path.join(WIN.DIR_FONTS, "grand9Kpixel.ttf"), size)
 
-
+#Load main menu music file
+pygame.mixer.music.load("Main_Menu_Music.mp3")
+#Loop through the music indefinitely
+pygame.mixer.play(-1)
 
 #If user clicks play, this is a place holder for now until game is ready
 def play():
