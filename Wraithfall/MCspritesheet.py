2efd8f0b97f6f281b20b850ea04f69031fb403bc
hattl11 <160966269+hattl11@users.noreply.github.com>
2024-04-21 23:57:23 -0500

    Added emotive portraits to the textbox

--- Wraithfall/MCspritesheet.py
+++ Wraithfall/MCspritesheet.py
@@ -8,7 +8,7 @@ SCREEN_HEIGHT = 500
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Sprite sheets")
 
-sprite_sheet_image = pygame.image.load(os.path.join(WIN.DIR_SPRITES, "MCSPRITESHEET.PNG")).convert_alpha()
+sheet = pygame.image.load(os.path.join(WIN.DIR_SPRITES, "MCSPRITESHEET.PNG")).convert_alpha()
 
 BG = (50, 50, 50)
 
@@ -21,18 +21,18 @@ def get_image(sheet, frame, width, height, scale):
     return image
 
 
-frame_0 = get_image(sprite_sheet_image, 0, 14, 17, 3)
-frame_1 = get_image(sprite_sheet_image, 1, 14, 17, 3)
-frame_2 = get_image(sprite_sheet_image, 2, 14, 17, 3)
-frame_3 = get_image(sprite_sheet_image, 3, 14, 17, 3)
-frame_4 = get_image(sprite_sheet_image, 4, 14, 17, 3)
-frame_5 = get_image(sprite_sheet_image, 5, 14, 17, 3)
-frame_6 = get_image(sprite_sheet_image, 6, 14, 17, 3)
-frame_7 = get_image(sprite_sheet_image, 7, 14, 17, 3)
-frame_8 = get_image(sprite_sheet_image, 8, 14, 17, 3)
-frame_9 = get_image(sprite_sheet_image, 9, 14, 17, 3)
-frame_10 = get_image(sprite_sheet_image, 10, 14, 17, 3)
-frame_11 = get_image(sprite_sheet_image, 11, 14, 17, 3)
+frame_0 = get_image(sheet, 0, 14, 17, 3)
+frame_1 = get_image(sheet, 1, 14, 17, 3)
+frame_2 = get_image(sheet, 2, 14, 17, 3)
+frame_3 = get_image(sheet, 3, 14, 17, 3)
+frame_4 = get_image(sheet, 4, 14, 17, 3)
+frame_5 = get_image(sheet, 5, 14, 17, 3)
+frame_6 = get_image(sheet, 6, 14, 17, 3)
+frame_7 = get_image(sheet, 7, 14, 17, 3)
+frame_8 = get_image(sheet, 8, 14, 17, 3)
+frame_9 = get_image(sheet, 9, 14, 17, 3)
+frame_10 = get_image(sheet, 10, 14, 17, 3)
+frame_11 = get_image(sheet, 11, 14, 17, 3)
 
 run = True
 while run:
--- Wraithfall/text_boxes.py
+++ Wraithfall/text_boxes.py
@@ -1,4 +1,8 @@
-import pygame, sys, os, game_window as WIN
+import game_window as WIN
+import os
+import pygame
+import sys
+import MCportraitsheet
 
 pygame.init()
 
@@ -6,9 +10,35 @@ pygame.init()
 SCREEN = pygame.display.set_mode(WIN.window_size())
 pygame.display.set_caption("Menu")
 
+
+def get_image(sheet, frame, width, height, scale):
+    image = pygame.Surface((width, height), pygame.SRCALPHA).convert_alpha()
+    image.blit(sheet, (0, 0), ((frame * width), 0, width, height))
+    image = pygame.transform.scale(image, (width * scale, height * scale))
+    return image
+
+
+# Dictionary mapping character names to portrait frames
+character_portraits = {
+    "MainCharacter": {
+        "Neutral": get_image(MCportraitsheet.sprite_sheet_image, 0, 104, 111, 2),
+        "Happy": get_image(MCportraitsheet.sprite_sheet_image, 1, 104, 111, 2),
+        "Excited": get_image(MCportraitsheet.sprite_sheet_image, 2, 104, 111, 2),
+        "Sad": get_image(MCportraitsheet.sprite_sheet_image, 3, 104, 111, 2),
+        "Angry": get_image(MCportraitsheet.sprite_sheet_image, 4, 104, 111, 2)
+    }
+}
+
+
+# Function to get the correct frame for a character's emotion
+def get_character_frame(character_name, emotion):
+    return character_portraits[character_name][emotion]
+
+
 # Load font
 def get_font(size):
     return pygame.font.Font(os.path.join(WIN.DIR_FONTS, "grand9Kpixel.ttf"), size)
+
 
 # Font size
 font_size = 20
@@ -23,35 +53,54 @@ text_lines = [
 # Index to keep track of the characters
 current_line_index = 0
 text_to_display = text_lines[current_line_index]
-#index to keep track of the characters
+# index to keep track of the characters
 text_index = 0
 # Time delay between the display of each character (in seconds)
 text_delay = 0.1
 
-#Load the sound file
+# Load the sound file
 pygame.mixer.init()
 sound = pygame.mixer.Sound(os.path.join(WIN.DIR_MUSIC, "text_sound.wav"))
-#play sound and loop until text is finished
+# play sound and loop until text is finished
 sound.play(loops=-1)
 
 # Add a clock to control the frame rate
 clock = pygame.time.Clock()
+
 
 def draw_text_box():
     # Define dimensions and position of the textbox
     text_box_width = WIN.WIN_WIDTH - 40
     text_box_height = font_size + 100
     text_box_rect = pygame.Rect((20, WIN.WIN_HEIGHT - text_box_height - 20), (text_box_width, text_box_height))
-    
+
     pygame.draw.rect(SCREEN, "white", text_box_rect, 2)
+
+    # Get the character name
+    character_name = "MainCharacter"
+
+    # Get the emotion based on the current line of text
+    if "Welcome" in text_to_display:
+        emotion = "Happy"
+    elif "Press Enter" in text_to_display:
+        emotion = "Excited"
+    else:
+        emotion = "Neutral"  # Default emotion
+
+    # Get the character portrait for the current emotion
+    portrait = get_character_frame(character_name, emotion)
+    if portrait:
+        portrait_rect = portrait.get_rect(topleft=(25, WIN.WIN_HEIGHT - text_box_height - 95))
+        SCREEN.blit(portrait, portrait_rect)
 
     # Center the text within the text box
     text_surface = font.render(text_to_display[:text_index + 1], True, "white")
     text_rect = text_surface.get_rect(center=text_box_rect.center)
-    
+
     # Blit the text surface onto the screen
     SCREEN.blit(text_surface, text_rect)
-    
+
+
 # Main loop
 running = True
 while running:
@@ -62,12 +111,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            #If enter key is pressed move to the next line
+            # If enter key is pressed move to the next line
             if event.key == pygame.K_RETURN:
                 text_index = 0
-                current_line_index = (current_line_index + 1) % len (text_lines)
+                current_line_index = (current_line_index + 1) % len(text_lines)
                 text_to_display = text_lines[current_line_index]
-                #Play sound when text is displayed
+                # Play sound when text is displayed
                 sound.play()
 
     draw_text_box()  # Draw the outline of the text box
@@ -76,9 +125,9 @@ while running:
     if pygame.time.get_ticks() % (text_delay * 40) == 0 and text_index < len(text_to_display):
         # Increment the index to display the next character
         text_index += 1
-        
-    #Stop sound once all text is displayed
-    if text_index >= len (text_to_display):
+
+    # Stop sound once all text is displayed
+    if text_index >= len(text_to_display):
         sound.stop()
 
     # Update the display
