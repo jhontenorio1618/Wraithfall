4563d23e86d522a196e8e6d8931d189a584674ed
mick2j <magrove2j@gmail.com>
2024-04-22 21:25:50 -0500

    cave map again

---
+++ Wraithfall/Map/r.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="r" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="TileCraftGroundSetVersion2(1).png" width="144" height="144"/>
+</tileset>
---
+++ Wraithfall/Map/y.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="y" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="tileset_cave_1.png" width="1024" height="1024"/>
+</tileset>
