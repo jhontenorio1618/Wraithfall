9fa2d1d6e9d7e4ece9af3a54086d31642e8e9bce
mcrank1 <mcrank@luc.edu>
2024-04-23 17:40:08 -0500

    Fixed bug where only the first character of each index was showing.

--- Wraithfall/FirstCinematic.py
+++ Wraithfall/FirstCinematic.py
@@ -3,10 +3,11 @@ import game_window as WIN
 import os
 import view_portraits
 import textbox
+import sys
 
 pygame.init()
 
-# Set screen size using the dimensions in the window_size function in game_window
+#Set screen size using the dimensions in the window_size function in game_window
 SCREEN = pygame.display.set_mode(WIN.window_size())
 pygame.display.set_caption("Menu")
 
@@ -16,20 +17,18 @@ sound = pygame.mixer.Sound(os.path.join(WIN.DIR_MUSIC, "text_sound.wav"))
 # play sound and loop until text is finished
 sound.play(loops=-1)
 
-# Text lines
+#Text lines
 text_lines = [
     textbox.TextBox("Welcome to Wraitfall...", "MainCharacter", "Happy"),
     textbox.TextBox("Press Enter...", "MainCharacter", "Excited")
 ]
 
-# Initialize variables
+#initialize variables
 current_line_index = 0
-text_index = 0
-text_delay = 0.1
 clock = pygame.time.Clock()
-running = True
 
 # Main loop
+running = True
 while running:
     SCREEN.fill("black")
 
@@ -40,23 +39,21 @@ while running:
         elif event.type == pygame.KEYDOWN:
             # If enter key is pressed move to the next line
             if event.key == pygame.K_RETURN:
-                text_index = 0
                 current_line_index = (current_line_index + 1) % len(text_lines)
                 current_text_line = text_lines[current_line_index]
+                current_text_line.text_index = 0
                 # Play sound when text is displayed
                 sound.play()
 
-    # Draw text box
+    #draw text box
     current_text_line = text_lines[current_line_index]
     current_text_line.draw(SCREEN)
-
-    # Delay the characters before displaying them
-    if pygame.time.get_ticks() % (text_delay * 40) == 0 and text_index < len(current_text_line.text):
-        # Increment the index to display the next character
-        text_index += 1
+    
+    # Update text index base on time
+    current_text_line.update()
 
     # Stop sound once all text is displayed
-    if text_index >= len(current_text_line.text):
+    if current_line_index >= len(current_text_line.text):
         sound.stop()
 
     # Update the display
