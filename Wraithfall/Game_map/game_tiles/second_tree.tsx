c8696bfbecfbac32b0d08a1b398d4f8015f692c8
jhontenorio1618 <jhontenorio1618@gmail.com>
2024-04-23 18:31:19 -0500

    Second Map

---
+++ Wraithfall/Game_map/game_tiles/second_tree.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="second_tree" tilewidth="224" tileheight="272" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="224" height="272" source="../game_images/second_tree.png"/>
+ </tile>
+</tileset>
