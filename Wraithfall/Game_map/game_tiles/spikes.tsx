f3595cfcb8cae9ecf0e2205c19d7fa3f75051f8f
jhontenorio1618 <jhontenorio1618@gmail.com>
2024-04-24 17:20:17 -0500

    new changes

--- Wraithfall/Game_map/game_tiles/spikes.tsx
+++ Wraithfall/Game_map/game_tiles/spikes.tsx
@@ -1,2 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="spikes" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.10.2" name="spikes" tilewidth="310" tileheight="311" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="310" height="311" source="../game_images/spikes.png"/>
+ </tile>
+</tileset>
